7badd71a7646036a3dee839173eda04539712a06
Omer Faruk Genc <ogenc@k1m32s03.42istanbul.com.tr>
2023-08-30 04:02:49 +0300

    coding minishell

--- new_minishell/main.c
+++ new_minishell/main.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ogenc <ogenc@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/30 03:56:43 by ogenc             #+#    #+#             */
+/*   Updated: 2023/08/30 03:56:44 by ogenc            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include<unistd.h>
@@ -20,11 +32,31 @@ int	find_env_dir(char **envp, char *find)
 	return (-1);
 }
 
+void	free_commands(char **commands)
+{
+	int x = -1;
+	while (commands[++x])
+		free(commands[x]);
+	free(commands);
+}
+
+// TODO:
+// env pathten aldıgım path verisinin bak: PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/munki
+// buradaki pathin içerisindeki ':' leri split ile ayırarak
+// access fonksiyonu ile girilen argümanın (örn ls) komutunun varlıgı kontrol edilecek
+// var olan yolun tamamı alınıp execve ile çalıştırılacak örnek /bin/ içerisinde ls varlıgı kontrol edildi
+// ve accessin döndürdügü dönüş degeri sayesinde /bin/ içerisinde oldugu dogrulandı execve ile çalıştırıldı.
+// TODO 2:
+// builtin fonksiyonları yazılacak, ayırılacak
+// cd komutunun tek "cd" argümanında home dizini
+
+
 int main(int argc, char **argv, char **envp)
 {
 	char *input;
 	char **commands;
 	char *path;
+	char **env_path;
 
 	while (1)
 	{
@@ -37,15 +69,17 @@ int main(int argc, char **argv, char **envp)
 				add_history(input);
 			if (ft_strncmp(commands[0], "exit",4) == 0)
 			{
-				printf("\033[31mExiting minishell...\n");
-				free(commands);
+				printf("\033[31mExiting minishell...\033[0m\n");
+				free_commands(commands);
 				free(input);
+				system("leaks a.out");
 				exit(0);
 			}
 			if (ft_strncmp(commands[0],"cd", 2) == 0)
 			{
 				if (commands[1])
-					chdir(commands[1]);
+					if (chdir(commands[1]) == -1)
+						printf("\033[31mcd: no such file or directory: %s\033[0m\n", commands[1]);
 			}
 			else
 			{
@@ -62,5 +96,8 @@ int main(int argc, char **argv, char **envp)
 			}
 		}
 		free(input);
+		free_commands(commands);
+		free(path);
 	}
 }
+
