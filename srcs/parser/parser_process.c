8f53597abaf24ef6405006001174efd826843963
segurbuz <sgurbuzofficial@gmail.com>
2023-09-16 18:03:11 +0300

    parser bug fixed

--- srcs/parser/parser_process.c
+++ srcs/parser/parser_process.c
@@ -121,15 +121,17 @@ char	*path_add_dollars(char *str, char *path, char *dollar)
 				path = ft_strjoin(dollar, path);
 			else
 				path = path_find(path);
+			break ;
 		}
 	}
 	new_str = ft_strjoin(str_start, path);
 	new_str = ft_strjoin(new_str, str_end);
-	free(dollar);
+	free(g_data.line);
+	g_data.line = new_str;
 	return (new_str);
 }
 
-char	*find_path_name(char *str)
+void	find_path_name()
 {
 	int		i;
 	int		len;
@@ -137,35 +139,34 @@ char	*find_path_name(char *str)
 
 	i = -1;
 	len = 1;
-	while (str[++i] != '\0')
+	while (g_data.line[++i] != '\0')
 	{
-		if (str[i] == '$')
+		if (g_data.line[i - 1] == '$')
 		{
-			i++;
-			while (len++ && path_check(str[i]))
+			len = 1;
+			while (len++ && path_check(g_data.line[i]))
 			{
-				if (!path_check(str[i]))
+				if (!path_check(g_data.line[i]))
 					break ;
 				i++;
 			}
-			path = ft_substr(str, (i - len) + 2, len - 2);
+			path = ft_substr(g_data.line, (i - len) + 2, len - 2);
+			path_add_dollars(g_data.line, path, ft_strdup("$"));
 		}
 	}
-	g_data.quot_type = 1000;
-	g_data.quot = 0;
-	return (path_add_dollars(str, path, ft_strdup("$")));
 }
 
 void	ft_parse(void)
 {
 	t_arg	*temp;
 	t_arg	*tmp;
-	char	*str;
 
 	temp = malloc(sizeof(t_arg));
 	temp->next = NULL;
-	str = find_path_name(ft_strtrim(g_data.line, " "));
-	split_line(temp, ft_strtrim(str, " "));
+	find_path_name();
+	g_data.quot_type = 1000;
+	g_data.quot = 0;
+	split_line(temp, ft_strtrim(g_data.line, " "));
 	if (g_data.error_flag == -1)
 	{
 		printf("HATA\n");
